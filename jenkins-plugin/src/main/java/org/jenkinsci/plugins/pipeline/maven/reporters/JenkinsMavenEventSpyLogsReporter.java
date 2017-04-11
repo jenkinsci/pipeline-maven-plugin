eea8e9ee6ce39ff3879f4156c039efab108bcc85
Cyrille Le Clerc <cleclerc@cloudbees.com>
2017-04-11 10:32:20 +0200

    [JENKINS-43491] Feature flag ".archive-jenkins-maven-event-spy-logs" to archive the logs file generated by the Jenkins Maven Event Spy. Primarily intended for troubleshooting purpose. The ".archive-jenkins-maven-event-spy-logs" has to be created in the Jenkins build workspace.

---
+++ jenkins-plugin/src/main/java/org/jenkinsci/plugins/pipeline/maven/reporters/JenkinsMavenEventSpyLogsReporter.java
@@ -0,0 +1,50 @@
+package org.jenkinsci.plugins.pipeline.maven.reporters;
+
+import hudson.FilePath;
+import hudson.Launcher;
+import hudson.model.Run;
+import hudson.model.TaskListener;
+import jenkins.model.ArtifactManager;
+import jenkins.util.BuildListenerAdapter;
+import org.jenkinsci.plugins.workflow.steps.StepContext;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collections;
+import java.util.Map;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Primarily for debugging purpose, archive the Maven build logs
+ *
+ * @author <a href="mailto:cleclerc@cloudbees.com">Cyrille Le Clerc</a>
+ */
+public class JenkinsMavenEventSpyLogsReporter {
+    public void process(@Nonnull StepContext context, @Nonnull FilePath mavenSpyLogs) throws IOException, InterruptedException {
+
+        Run run = context.get(Run.class);
+        ArtifactManager artifactManager = run.pickArtifactManager();
+        Launcher launcher = context.get(Launcher.class);
+        TaskListener listener = context.get(TaskListener.class);
+        FilePath workspace = context.get(FilePath.class);
+
+        // ARCHIVE MAVEN BUILD LOGS
+        FilePath tmpFile = new FilePath(workspace, "." + mavenSpyLogs.getName());
+        try {
+            mavenSpyLogs.copyTo(tmpFile);
+            listener.getLogger().println("[withMaven] archive " + mavenSpyLogs.getRemote() + " as " + mavenSpyLogs.getName());
+            // filePathInArchiveZone -> filePathInWorkspace
+            Map<String, String> mavenBuildLogs = Collections.singletonMap(mavenSpyLogs.getName(), tmpFile.getName());
+            artifactManager.archive(workspace, launcher, new BuildListenerAdapter(listener), mavenBuildLogs);
+        } catch (Exception e) {
+            PrintWriter errorWriter = listener.error("[withMaven] WARNING Exception archiving Maven build logs " + mavenSpyLogs + ", skip file. ");
+            e.printStackTrace(errorWriter);
+        } finally {
+            boolean deleted = tmpFile.delete();
+            if (!deleted) {
+                listener.error("[withMaven] WARNING Failure to delete temporary file " + tmpFile);
+            }
+        }
+    }
+}
