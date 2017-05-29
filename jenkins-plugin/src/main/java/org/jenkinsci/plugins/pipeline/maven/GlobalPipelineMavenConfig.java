3bee359efb77f78f164422697193439f0f7c9478
Cyrille Le Clerc <cleclerc@cloudbees.com>
2017-05-29 10:43:01 +0200

    [JENKINS-44384] Support configuration of withMaven publishers in the Jenkins “Global Tools Configuration” section.

---
+++ jenkins-plugin/src/main/java/org/jenkinsci/plugins/pipeline/maven/GlobalPipelineMavenConfig.java
@@ -0,0 +1,66 @@
+package org.jenkinsci.plugins.pipeline.maven;
+
+import hudson.Extension;
+import jenkins.model.GlobalConfiguration;
+import jenkins.model.GlobalConfigurationCategory;
+import jenkins.tools.ToolConfigurationCategory;
+import net.sf.json.JSONObject;
+import org.jenkinsci.Symbol;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.DataBoundSetter;
+import org.kohsuke.stapler.StaplerRequest;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.annotation.CheckForNull;
+import javax.annotation.Nullable;
+
+/**
+ * @author <a href="mailto:cleclerc@cloudbees.com">Cyrille Le Clerc</a>
+ */
+@Extension(ordinal = 50) @Symbol("pipelineMaven")
+public class GlobalPipelineMavenConfig extends GlobalConfiguration {
+
+    private final static Logger LOGGER = Logger.getLogger(GlobalPipelineMavenConfig.class.getName());
+
+    @DataBoundConstructor
+    public GlobalPipelineMavenConfig() {
+        load();
+    }
+
+    @Override
+    public ToolConfigurationCategory getCategory() {
+        return GlobalConfigurationCategory.get(ToolConfigurationCategory.class);
+    }
+
+    private List<MavenPublisher> publisherOptions;
+
+    @CheckForNull
+    public List<MavenPublisher> getPublisherOptions() {
+        return publisherOptions;
+    }
+
+    @DataBoundSetter
+    public void setPublisherOptions(List<MavenPublisher> publisherOptions) {
+        this.publisherOptions = publisherOptions;
+        save();
+    }
+
+    public void setPublisherOptions(MavenPublisher... publishers) {
+        this.publisherOptions = Arrays.asList(publishers);
+        save();
+    }
+
+    @Override
+    public boolean configure(StaplerRequest req, JSONObject json) throws FormException {
+        req.bindJSON(this, json);
+        return true;
+    }
+
+    @Nullable
+    public static GlobalPipelineMavenConfig get() {
+        return GlobalConfiguration.all().get(GlobalPipelineMavenConfig.class);
+    }
+}
--- jenkins-plugin/src/main/java/org/jenkinsci/plugins/pipeline/maven/MavenPublisher.java
+++ jenkins-plugin/src/main/java/org/jenkinsci/plugins/pipeline/maven/MavenPublisher.java
@@ -6,13 +6,18 @@ import hudson.model.AbstractDescribableImpl;
 import hudson.model.Descriptor;
 import hudson.model.TaskListener;
 import jenkins.model.Jenkins;
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.beanutils.BeanUtilsBean2;
+import org.apache.commons.beanutils.PropertyUtils;
 import org.jenkinsci.plugins.workflow.steps.StepContext;
 import org.kohsuke.stapler.DataBoundSetter;
 import org.w3c.dom.Element;
 
+import java.beans.PropertyDescriptor;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.Serializable;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -34,16 +39,14 @@ public abstract class MavenPublisher extends AbstractDescribableImpl<MavenPublis
 
     private final static Logger LOGGER = Logger.getLogger(MavenPublisher.class.getName());
 
-    @CheckForNull
-    private Boolean disabled;
+    private boolean disabled;
 
-    @CheckForNull
-    public Boolean isDisabled() {
+    public boolean isDisabled() {
         return disabled;
     }
 
     @DataBoundSetter
-    public void setDisabled(@Nullable Boolean disabled) {
+    public void setDisabled(boolean disabled) {
         this.disabled = disabled;
     }
 
@@ -74,7 +77,6 @@ public abstract class MavenPublisher extends AbstractDescribableImpl<MavenPublis
 
     public static abstract class DescriptorImpl extends Descriptor<MavenPublisher> implements Comparable<DescriptorImpl> {
         /**
-         *
          * @return the ordinal of this reporter to execute publishers in predictable order
          * @see #compareTo(MavenPublisher)
          */
@@ -103,46 +105,125 @@ public abstract class MavenPublisher extends AbstractDescribableImpl<MavenPublis
         }
     }
 
+    /**
+     * <p>Build the list of {@link MavenPublisher}s that should be invoked for the build execution of the given {@link TaskListener}
+     * with the desired configuration.
+     * </p>
+     * <p>
+     * The desired configuration is based on:
+     * </p>
+     * <ul>
+     * <li>The default configuration of the publishers</li>
+     * <li>The global configuration of the publishers defined in the "Global Tools Configuration' section</li>
+     * <li>The configuration specified in the {@code withMaven(options=[...])} step</li>
+     * </ul>
+     *
+     * @param configuredPublishers
+     * @param listener
+     */
     @Nonnull
-    public static List<MavenPublisher> buildReportersList(@Nonnull List<MavenPublisher> configuredReporters, @Nonnull TaskListener listener){
+    public static List<MavenPublisher> buildPublishersList(@Nonnull List<MavenPublisher> configuredPublishers, @Nonnull TaskListener listener) {
 
-        // mavenReporter.descriptor.id -> mavenReporter
-        Map<String, MavenPublisher> configuredReportersById = new HashMap<>();
-        for (MavenPublisher mavenPublisher : configuredReporters) {
+        // configuration passed as parameter of "withMaven(options=[...]){}"
+        // mavenPublisher.descriptor.id -> mavenPublisher
+        Map<String, MavenPublisher> configuredPublishersById = new HashMap<>();
+        for (MavenPublisher mavenPublisher : configuredPublishers) {
             if (mavenPublisher == null) {
-                // skipp null reporter injected by Jenkins pipeline for an unknown reason
+                // skip null publisher options injected by Jenkins pipeline, probably caused by a missing plugin
             } else {
-                configuredReportersById.put(mavenPublisher.getDescriptor().getId(), mavenPublisher);
+                configuredPublishersById.put(mavenPublisher.getDescriptor().getId(), mavenPublisher);
             }
         }
 
-        // mavenReporter.descriptor.id -> mavenRepoer
-        Map<String, MavenPublisher> defaultReportersById = new HashMap<>();
+        // configuration defined globally
+        Map<String, MavenPublisher> globallyConfiguredPublishersById = new HashMap<>();
+        GlobalPipelineMavenConfig globalPipelineMavenConfig = GlobalPipelineMavenConfig.get();
+
+        List<MavenPublisher> globallyConfiguredPublishers = globalPipelineMavenConfig == null ? Collections.<MavenPublisher>emptyList() : globalPipelineMavenConfig.getPublisherOptions();
+        if (globallyConfiguredPublishers == null) {
+            globallyConfiguredPublishers = Collections.emptyList();
+        }
+        for (MavenPublisher mavenPublisher : globallyConfiguredPublishers) {
+            globallyConfiguredPublishersById.put(mavenPublisher.getDescriptor().getId(), mavenPublisher);
+        }
+
+
+        // mavenPublisher.descriptor.id -> mavenPublisher
+        Map<String, MavenPublisher> defaultPublishersById = new HashMap<>();
         DescriptorExtensionList<MavenPublisher, Descriptor<MavenPublisher>> descriptorList = Jenkins.getInstance().getDescriptorList(MavenPublisher.class);
-        for (Descriptor<MavenPublisher> descriptor:descriptorList) {
-            if (configuredReportersById.containsKey(descriptor.getId())) {
-                // skip, already provided with a configuration
-            } else {
-                try {
-                    defaultReportersById.put(descriptor.getId(), descriptor.clazz.newInstance());
-                } catch (InstantiationException | IllegalAccessException e) {
-                    PrintWriter error = listener.error("[withMaven] Exception instantiation default config for Maven Reporter '" + descriptor.getDisplayName() + "' / " + descriptor.getId() + ": " + e);
-                    e.printStackTrace(error);
-                    error.close();
-                    LOGGER.log(Level.WARNING, "Exception instantiating " + descriptor.clazz + ": " + e, e);
-                    e.printStackTrace();
-                }
+        for (Descriptor<MavenPublisher> descriptor : descriptorList) {
+            try {
+                defaultPublishersById.put(descriptor.getId(), descriptor.clazz.newInstance());
+            } catch (InstantiationException | IllegalAccessException e) {
+                PrintWriter error = listener.error("[withMaven] Exception instantiation default config for Maven Publisher '" + descriptor.getDisplayName() + "' / " + descriptor.getId() + ": " + e);
+                e.printStackTrace(error);
+                error.close();
+                LOGGER.log(Level.WARNING, "Exception instantiating " + descriptor.clazz + ": " + e, e);
+                e.printStackTrace();
             }
         }
+
+
         if (LOGGER.isLoggable(Level.FINE)) {
-            listener.getLogger().println("[withMaven] Maven Reporters with configuration provided by the pipeline: " + configuredReportersById);
-            listener.getLogger().println("[withMaven] Maven Reporters with default configuration: " + defaultReportersById);
+            listener.getLogger().println("[withMaven] Maven Publishers with configuration provided by the pipeline: " + configuredPublishersById.values());
+            listener.getLogger().println("[withMaven] Maven Publishers with configuration defined globally: " + globallyConfiguredPublishersById.values());
+            listener.getLogger().println("[withMaven] Maven Publishers with default configuration: " + defaultPublishersById.values());
         }
 
+        // TODO FILTER
         List<MavenPublisher> results = new ArrayList<>();
-        results.addAll(configuredReportersById.values());
-        results.addAll(defaultReportersById.values());
+        for (Map.Entry<String, MavenPublisher> entry : defaultPublishersById.entrySet()) {
+            String publisherId = entry.getKey();
+            MavenPublisher publisher = buildConfiguredMavenPublisher(
+                    configuredPublishersById.get(publisherId),
+                    globallyConfiguredPublishersById.get(publisherId),
+                    entry.getValue(),
+                    listener);
+            results.add(publisher);
+        }
         Collections.sort(results);
         return results;
+    }
+
+    public static MavenPublisher buildConfiguredMavenPublisher(@Nullable MavenPublisher pipelinePublisher, @Nullable MavenPublisher globallyConfiguredPublisher, @Nonnull MavenPublisher defaultPublisher, @Nonnull TaskListener listener) {
+
+        MavenPublisher result;
+        String logMessage;
+
+        if (pipelinePublisher == null && globallyConfiguredPublisher == null) {
+            result = defaultPublisher;
+            logMessage = "default";
+        } else if (pipelinePublisher == null && globallyConfiguredPublisher != null) {
+            result = globallyConfiguredPublisher;
+            logMessage = "globally";
+        } else if (pipelinePublisher != null && globallyConfiguredPublisher == null) {
+            result = pipelinePublisher;
+            logMessage = "pipeline";
+        } else if (pipelinePublisher != null && globallyConfiguredPublisher != null)  {
+            // workaround FindBugs "Bug kind and pattern: NP - NP_NULL_ON_SOME_PATH"
+            // check pipelinePublisher and globallyConfiguredPublisher are non null even if it is useless
+
+            result = pipelinePublisher;
+            logMessage = "pipeline";
+            listener.getLogger().println("[withMaven] WARNING merging publisher configuration defined in the 'Global Tool Configuration' and at the pipeline level is not yet supported." +
+                    " Use pipeline level configuration for '" + result.getDescriptor().getDisplayName() + "'");
+//
+//            PropertyDescriptor[] propertyDescriptors = PropertyUtils.getPropertyDescriptors(defaultPublisher);
+//            for(PropertyDescriptor propertyDescriptor: propertyDescriptors) {
+//                Method readMethod = propertyDescriptor.getReadMethod();
+//                Method writeMethod = propertyDescriptor.getWriteMethod();
+//
+//                Object defaultValue = readMethod.invoke(defaultPublisher);
+//                Object globallyDefinedValue = readMethod.invoke(globallyConfiguredPublisher);
+//                Object pipelineValue = readMethod.invoke(pipelinePublisher);
+//            }
+        } else {
+            throw new IllegalStateException("Should not happen, workaround for Findbugs NP_NULL_ON_SOME_PATH above");
+        }
+
+        if (LOGGER.isLoggable(Level.FINE))
+            listener.getLogger().println("[withMaven] Use " + logMessage + " defined publisher for '" + result.getDescriptor().getDisplayName() + "'");
+        return result;
+
     }
 }
