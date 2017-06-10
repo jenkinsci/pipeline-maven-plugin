b3e48c2b176349ed16ed5d80609e1d090d897496
Cyrille Le Clerc <cleclerc@cloudbees.com>
2017-06-10 19:35:56 +0200

    [JENKINS-43094] Add ATH unit test tests

---
+++ jenkins-plugin/src/test/resources/org/jenkinsci/plugins/pipeline/maven/test/test_maven_projects/mono_dependency_maven_jar_project/src/main/java/com/example/App.java
@@ -0,0 +1,10 @@
+package com.example;
+
+/**
+ * @author <a href="mailto:cleclerc@cloudbees.com">Cyrille Le Clerc</a>
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("App");
+    }
+}
