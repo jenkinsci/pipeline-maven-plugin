902ced6df60a863c2ebe94d7105eca344a094739
Cyrille Le Clerc <cleclerc@cloudbees.com>
2018-11-08 07:04:21 +0900

    JENKINS-54437 add "validate jdbc connection" button

--- jenkins-plugin/src/main/java/org/jenkinsci/plugins/pipeline/maven/GlobalPipelineMavenConfig.java
+++ jenkins-plugin/src/main/java/org/jenkinsci/plugins/pipeline/maven/GlobalPipelineMavenConfig.java
@@ -28,12 +28,12 @@ import com.cloudbees.plugins.credentials.CredentialsMatchers;
 import com.cloudbees.plugins.credentials.CredentialsProvider;
 import com.cloudbees.plugins.credentials.common.StandardListBoxModel;
 import com.cloudbees.plugins.credentials.common.UsernamePasswordCredentials;
-import com.google.common.base.Strings;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import hudson.Extension;
 import hudson.model.Result;
 import hudson.security.ACL;
+import hudson.util.FormValidation;
 import hudson.util.ListBoxModel;
 import hudson.util.Secret;
 import jenkins.model.GlobalConfiguration;
@@ -41,32 +41,39 @@ import jenkins.model.GlobalConfigurationCategory;
 import jenkins.model.Jenkins;
 import jenkins.tools.ToolConfigurationCategory;
 import net.sf.json.JSONObject;
-import org.h2.jdbcx.JdbcConnectionPool;
+import org.apache.commons.lang.StringUtils;
 import org.jenkinsci.Symbol;
 import org.jenkinsci.plugins.pipeline.maven.dao.PipelineMavenPluginDao;
 import org.jenkinsci.plugins.pipeline.maven.dao.PipelineMavenPluginH2Dao;
-import org.jenkinsci.plugins.pipeline.maven.dao.PipelineMavenPluginH2v1Dao;
 import org.jenkinsci.plugins.pipeline.maven.dao.PipelineMavenPluginMonitoringDao;
 import org.jenkinsci.plugins.pipeline.maven.dao.PipelineMavenPluginMySqlDao;
 import org.jenkinsci.plugins.pipeline.maven.dao.PipelineMavenPluginNullDao;
 import org.jenkinsci.plugins.pipeline.maven.service.PipelineTriggerService;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
+import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Properties;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.annotation.CheckForNull;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.sql.DataSource;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.StringReader;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * @author <a href="mailto:cleclerc@cloudbees.com">Cyrille Le Clerc</a>
@@ -196,7 +203,7 @@ public class GlobalPipelineMavenConfig extends GlobalConfiguration {
         if (dao == null) {
             try {
                 String jdbcUrl, jdbcUserName, jdbcPassword;
-                if (Strings.isNullOrEmpty(this.jdbcUrl)) {
+                if (StringUtils.isBlank(this.jdbcUrl)) {
                     // default embedded H2 database
                     File databaseRootDir = new File(Jenkins.getInstance().getRootDir(), "jenkins-jobs");
                     if (!databaseRootDir.exists()) {
@@ -230,7 +237,7 @@ public class GlobalPipelineMavenConfig extends GlobalConfiguration {
                 dsConfig.setUsername(jdbcUserName);
                 dsConfig.setPassword(jdbcPassword);
 
-                if (!Strings.isNullOrEmpty(properties)) {
+                if (StringUtils.isNotBlank(properties)) {
                     Properties p = new Properties();
                     p.load(new StringReader(properties));
                     dsConfig.setDataSourceProperties(p);
@@ -305,5 +312,80 @@ public class GlobalPipelineMavenConfig extends GlobalConfiguration {
                                 Jenkins.getInstance(),
                                 ACL.SYSTEM,
                                 Collections.EMPTY_LIST));
+    }
+    public FormValidation doValidateJdbcConnection(
+                                     @QueryParameter String jdbcUrl,
+                                     @QueryParameter String properties,
+                                     @QueryParameter String jdbcCredentialsId) {
+        if (StringUtils.isBlank(jdbcUrl)) {
+            return FormValidation.ok("OK");
+        }
+
+        String driverClass = null;
+        try {
+            if (StringUtils.isBlank(jdbcUrl)) {
+                driverClass = "org.h2.Driver";
+            } else if (jdbcUrl.startsWith("jdbc:h2")) {
+                driverClass = "org.h2.Driver";
+            } else if (jdbcUrl.startsWith("jdbc:mysql")) {
+                driverClass = "com.mysql.cj.jdbc.Driver";
+            } else {
+                return FormValidation.error("Unsupported database specified in JDBC url '" + jdbcUrl + "'");
+            }
+            Class.forName(driverClass);
+
+            String jdbcUserName, jdbcPassword;
+            if (StringUtils.isEmpty(jdbcCredentialsId)) {
+                if (StringUtils.isBlank(jdbcUrl)) {
+                    // embedded database, assume OK
+                    return FormValidation.ok("OK");
+                } else {
+                    return FormValidation.error("No credentials specified for JDBC url '" + jdbcUrl + "'");
+                }
+            } else {
+                UsernamePasswordCredentials jdbcCredentials = (UsernamePasswordCredentials) CredentialsMatchers.firstOrNull(
+                        CredentialsProvider.lookupCredentials(UsernamePasswordCredentials.class, Jenkins.getInstance(),
+                                ACL.SYSTEM, Collections.EMPTY_LIST),
+                        CredentialsMatchers.withId(jdbcCredentialsId));
+                if (jdbcCredentials == null) {
+                    return FormValidation.error("Credentials '" + jdbcCredentialsId + "' defined for JDBC URL '" + jdbcUrl + "' not found");
+                }
+                jdbcUserName = jdbcCredentials.getUsername();
+                jdbcPassword = Secret.toString(jdbcCredentials.getPassword());
+            }
+            HikariConfig dsConfig = new HikariConfig();
+            dsConfig.setJdbcUrl(jdbcUrl);
+            dsConfig.setUsername(jdbcUserName);
+            dsConfig.setPassword(jdbcPassword);
+
+            if (StringUtils.isNotBlank(properties)) {
+                Properties p = new Properties();
+                try {
+                    p.load(new StringReader(properties));
+                } catch (IOException e) {
+                    throw new IllegalStateException(e);
+                }
+                dsConfig.setDataSourceProperties(p);
+            }
+
+            try (HikariDataSource ds = new HikariDataSource(dsConfig)) {
+                try (Connection cnn = ds.getConnection()) {
+                    try (Statement stmt = cnn.createStatement()) {
+                        try (ResultSet rst = stmt.executeQuery("select 1")) {
+                            rst.next();
+                            // TODO more tests
+                        }
+                    }
+                } catch (SQLException e ){
+                    return FormValidation.error(e, "Failure to connect to the database " + jdbcUrl);
+                }
+            }
+            return FormValidation.ok("OK");
+        } catch (RuntimeException e) {
+            return FormValidation.error(e, "Failed to test JDBC connection '" + jdbcUrl + "'");
+        } catch (ClassNotFoundException e) {
+            return FormValidation.error(e, "Failed to load JDBC driver '" + driverClass + "' for JDBC connection '" + jdbcUrl + "'");
+        }
+
     }
 }
