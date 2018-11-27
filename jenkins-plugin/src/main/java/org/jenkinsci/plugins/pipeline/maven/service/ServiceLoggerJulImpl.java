2e10fefe01d8fa91667f70fa37c848d2eff0e3e6
Cyrille Le Clerc <cleclerc@cloudbees.com>
2018-11-27 14:55:12 +0100

    Better service logger for extensibility

--- jenkins-plugin/src/main/java/org/jenkinsci/plugins/pipeline/maven/service/ServiceLoggerJulImpl.java
+++ jenkins-plugin/src/main/java/org/jenkinsci/plugins/pipeline/maven/service/ServiceLoggerJulImpl.java
@@ -26,8 +26,6 @@ package org.jenkinsci.plugins.pipeline.maven.service;
 
 import hudson.model.Item;
 
-import java.io.PrintStream;
-import java.io.PrintWriter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -37,19 +35,12 @@ import javax.annotation.Nullable;
 /**
  * @author <a href="mailto:cleclerc@cloudbees.com">Cyrille Le Clerc</a>
  */
-public class ServiceLoggerImpl implements ServiceLogger {
+public class ServiceLoggerJulImpl implements ServiceLogger {
 
-    private final Logger logger = Logger.getLogger(ServiceLoggerImpl.class.getName());
+    private transient final Logger logger;
 
-    @Nonnull
-    private final PrintStream stdOut, stdErr;
-    @Nullable
-    String prefix;
-
-    public ServiceLoggerImpl(@Nonnull PrintStream stdOut, @Nonnull PrintStream stdErr, @Nullable String prefix) {
-        this.stdOut = stdOut;
-        this.stdErr = stdErr;
-        this.prefix = prefix;
+    public ServiceLoggerJulImpl(@Nonnull Logger logger) {
+        this.logger = logger;
     }
 
     @Override
@@ -59,17 +50,10 @@ public class ServiceLoggerImpl implements ServiceLogger {
 
     @Override
     public void log(Level level, String message) {
-        if (!isLoggable(level)) {
-            return;
-        }
-        StringBuilder messageToWrite = new StringBuilder();
-        if (prefix != null && ! prefix.isEmpty()) {
-            messageToWrite.append(prefix).append(" ");
-        }
-        messageToWrite.append(level).append(" ").append(message);
-        stdOut.println(messageToWrite.toString());
+        logger.log(level, message);
     }
 
+    @Nonnull
     @Override
     public String modelHyperlinkNoteEncodeTo(@Nullable Item item) {
         return item == null ? "#null#" : item.getFullDisplayName();
