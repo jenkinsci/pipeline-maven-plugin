jenkinsci/pipeline-maven-plugin

commit 5c82c80be34831adc581b6e3e2a5d71b20e6d67f

chore: do not depend on workflow-job for production code (#517)

jenkins-plugin/src/main/java/org/jenkinsci/plugins/pipeline/maven/listeners/DatabaseSyncItemListener.java
@@ -6,7 +6,7 @@ import hudson.model.ItemGroup;
 import hudson.model.listeners.ItemListener;
 import jenkins.model.Jenkins;
 import org.jenkinsci.plugins.pipeline.maven.GlobalPipelineMavenConfig;
-import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import org.jenkinsci.plugins.workflow.flow.FlowExecutionOwner;
 
 import javax.inject.Inject;
 import java.util.logging.Level;
@@ -23,58 +23,44 @@ public class DatabaseSyncItemListener extends ItemListener {
 
     @Inject
     public GlobalPipelineMavenConfig globalPipelineMavenConfig;
-    
+
     @Override
     public void onDeleted(Item item) {
-        if (item instanceof WorkflowJob) {
-            WorkflowJob pipeline = (WorkflowJob) item;
-            onDeleted(pipeline);
+        if (item instanceof FlowExecutionOwner.Executable) {
+            LOGGER.log(Level.FINE, "onDeleted({0})", item);
+            globalPipelineMavenConfig.getDao().deleteJob(item.getFullName());
         } else {
             LOGGER.log(Level.FINE, "Ignore onDeleted({0})", new Object[]{item});
         }
     }
 
-    public void onDeleted(WorkflowJob pipeline) {
-        LOGGER.log(Level.FINE, "onDeleted({0})", pipeline);
-        globalPipelineMavenConfig.getDao().deleteJob(pipeline.getFullName());
-    }
-
     @Override
     public void onRenamed(Item item, String oldName, String newName) {
-        if (item instanceof WorkflowJob) {
-            WorkflowJob pipeline = (WorkflowJob) item;
-            onRenamed(pipeline, oldName, newName);
+        if (item instanceof FlowExecutionOwner.Executable) {
+            LOGGER.log(Level.FINE, "onRenamed({0}, {1}, {2})", new Object[]{item, oldName, newName});
+
+            String oldFullName;
+            ItemGroup parent = item.getParent();
+            if (parent.equals(Jenkins.get())) {
+                oldFullName = oldName;
+            } else {
+                oldFullName = parent.getFullName() + "/" + oldName;
+            }
+            String newFullName = item.getFullName();
+            globalPipelineMavenConfig.getDao().renameJob(oldFullName, newFullName);
         } else {
             LOGGER.log(Level.FINE, "Ignore onRenamed({0}, {1}, {2})", new Object[]{item, oldName, newName});
         }
     }
 
-    public void onRenamed(WorkflowJob pipeline, String oldName, String newName) {
-        LOGGER.log(Level.FINE, "onRenamed({0}, {1}, {2})", new Object[]{pipeline, oldName, newName});
-
-        String oldFullName;
-        ItemGroup parent = pipeline.getParent();
-        if (parent.equals(Jenkins.get())) {
-            oldFullName = oldName;
-        } else {
-            oldFullName = parent.getFullName() + "/" + oldName;
-        }
-        String newFullName = pipeline.getFullName();
-        globalPipelineMavenConfig.getDao().renameJob(oldFullName, newFullName);
-    }
-
     @Override
     public void onLocationChanged(Item item, String oldFullName, String newFullName) {
-        if (item instanceof WorkflowJob) {
-            WorkflowJob pipeline = (WorkflowJob) item;
-            onLocationChanged(pipeline, oldFullName, newFullName);
+        if (item instanceof FlowExecutionOwner.Executable) {
+            LOGGER.log(Level.FINE, "onLocationChanged({0}, {1}, {2})", new Object[]{item, oldFullName, newFullName});
+            globalPipelineMavenConfig.getDao().renameJob(oldFullName, newFullName);
         } else {
             LOGGER.log(Level.FINE, "Ignore onLocationChanged({0}, {1}, {2})", new Object[]{item, oldFullName, newFullName});
         }
     }
 
-    public void onLocationChanged(WorkflowJob pipeline, String oldFullName, String newFullName) {
-        LOGGER.log(Level.FINE, "onLocationChanged({0}, {1}, {2})", new Object[]{pipeline, oldFullName, newFullName});
-        globalPipelineMavenConfig.getDao().renameJob(oldFullName, newFullName);
-    }
 }

jenkins-plugin/src/main/java/org/jenkinsci/plugins/pipeline/maven/trigger/WorkflowJobDependencyTrigger.java
@@ -7,7 +7,7 @@ import hudson.triggers.TriggerDescriptor;
 import jenkins.branch.MultiBranchProject;
 import jenkins.branch.OrganizationFolder;
 import org.jenkinsci.Symbol;
-import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import org.jenkinsci.plugins.workflow.flow.FlowExecutionOwner;
 import org.kohsuke.stapler.DataBoundConstructor;
 
 /**
@@ -25,7 +25,7 @@ public class WorkflowJobDependencyTrigger extends Trigger<Item> {
     public static class DescriptorImpl extends TriggerDescriptor {
         @Override
         public boolean isApplicable(Item item) {
-            return item instanceof WorkflowJob || item instanceof MultiBranchProject || item instanceof OrganizationFolder;
+            return item instanceof FlowExecutionOwner.Executable || item instanceof MultiBranchProject || item instanceof OrganizationFolder;
         }
 
         public String getDisplayName() {

jenkins-plugin/pom.xml
@@ -399,10 +399,6 @@
     </dependency>
     <dependency>
       <groupId>org.jenkins-ci.plugins.workflow</groupId>
-      <artifactId>workflow-job</artifactId>
-    </dependency>
-    <dependency>
-      <groupId>org.jenkins-ci.plugins.workflow</groupId>
       <artifactId>workflow-step-api</artifactId>
     </dependency>
     <dependency>
@@ -498,6 +494,11 @@
     <dependency>
       <groupId>org.jenkins-ci.plugins.workflow</groupId>
       <artifactId>workflow-durable-task-step</artifactId>
+      <scope>test</scope>
+    </dependency>
+    <dependency>
+      <groupId>org.jenkins-ci.plugins.workflow</groupId>
+      <artifactId>workflow-job</artifactId>
       <scope>test</scope>
     </dependency>
     <dependency>

jenkins-plugin/src/main/java/org/jenkinsci/plugins/pipeline/maven/db/migration/h2/MigrationStep11.java
@@ -1,11 +1,10 @@
 package org.jenkinsci.plugins.pipeline.maven.db.migration.h2;
 
 import hudson.model.Cause;
+import hudson.model.Job;
 import hudson.model.Run;
 import jenkins.model.Jenkins;
 import org.jenkinsci.plugins.pipeline.maven.db.migration.MigrationStep;
-import org.jenkinsci.plugins.workflow.job.WorkflowJob;
-import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -54,11 +53,11 @@ public class MigrationStep11 implements MigrationStep {
                         long jenkinsMasterId = rst.getLong("jenkins_master_id");
 
                         try {
-                            WorkflowJob pipeline = Jenkins.get().getItemByFullName(jobFullName, WorkflowJob.class);
+                            Job<?, ?> pipeline = Jenkins.get().getItemByFullName(jobFullName, Job.class);
                             if (pipeline == null) {
                                 continue;
                             }
-                            WorkflowRun build = pipeline.getBuildByNumber(buildNumber);
+                            Run<?, ?> build = pipeline.getBuildByNumber(buildNumber);
                             if (build == null) {
                                 continue;
                             }

jenkins-plugin/src/main/java/org/jenkinsci/plugins/pipeline/maven/listeners/DatabaseSyncRunListener.java
@@ -3,10 +3,10 @@ package org.jenkinsci.plugins.pipeline.maven.listeners;
 import hudson.Extension;
 import hudson.model.Cause;
 import hudson.model.Result;
+import hudson.model.Run;
 import hudson.model.TaskListener;
 import hudson.model.listeners.RunListener;
 import org.jenkinsci.plugins.pipeline.maven.GlobalPipelineMavenConfig;
-import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
@@ -15,18 +15,18 @@ import javax.inject.Inject;
  * @author <a href="mailto:cleclerc@cloudbees.com">Cyrille Le Clerc</a>
  */
 @Extension
-public class DatabaseSyncRunListener extends RunListener<WorkflowRun> {
+public class DatabaseSyncRunListener extends RunListener<Run<?, ?>> {
 
     @Inject
     public GlobalPipelineMavenConfig globalPipelineMavenConfig;
 
     @Override
-    public void onDeleted(WorkflowRun run) {
+    public void onDeleted(Run<?, ?> run) {
         globalPipelineMavenConfig.getDao().deleteBuild(run.getParent().getFullName(), run.getNumber());
     }
 
     @Override
-    public void onInitialize(WorkflowRun run) {
+    public void onInitialize(Run<?, ?> run) {
         super.onInitialize(run);
 
         for (Cause cause: run.getCauses()) {
@@ -45,7 +45,7 @@ public class DatabaseSyncRunListener extends RunListener<WorkflowRun> {
      * TODO ensure that this code executes before DownstreamPipelineTriggerRunListener#onCompleted
      */
     @Override
-    public void onCompleted(WorkflowRun workflowRun, @Nonnull TaskListener listener) {
+    public void onCompleted(Run<?, ?> workflowRun, @Nonnull TaskListener listener) {
         super.onCompleted(workflowRun, listener);
 
         // Note: run.duration is zero in onCompleted(), do the substraction in this listener

jenkins-plugin/src/main/java/org/jenkinsci/plugins/pipeline/maven/listeners/DownstreamPipelineTriggerRunListener.java
@@ -19,8 +19,6 @@ import org.jenkinsci.plugins.pipeline.maven.cause.MavenDependencyCauseHelper;
 import org.jenkinsci.plugins.pipeline.maven.cause.MavenDependencyUpstreamCause;
 import org.jenkinsci.plugins.pipeline.maven.cause.OtherMavenDependencyCause;
 import org.jenkinsci.plugins.pipeline.maven.trigger.WorkflowJobDependencyTrigger;
-import org.jenkinsci.plugins.workflow.job.WorkflowJob;
-import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
@@ -46,7 +44,7 @@ import java.util.stream.Collectors;
  * @author <a href="mailto:cleclerc@cloudbees.com">Cyrille Le Clerc</a>
  */
 @Extension
-public class DownstreamPipelineTriggerRunListener extends RunListener<WorkflowRun> {
+public class DownstreamPipelineTriggerRunListener extends RunListener<Run<?, ?>> {
 
     private final static Logger LOGGER = Logger.getLogger(DownstreamPipelineTriggerRunListener.class.getName());
 
@@ -54,7 +52,7 @@ public class DownstreamPipelineTriggerRunListener extends RunListener<WorkflowRu
     public GlobalPipelineMavenConfig globalPipelineMavenConfig;
 
     @Override
-    public void onCompleted(WorkflowRun upstreamBuild, @Nonnull TaskListener listener) {
+    public void onCompleted(Run<?, ?> upstreamBuild, @Nonnull TaskListener listener) {
         LOGGER.log(Level.FINER, "onCompleted({0})", new Object[]{upstreamBuild});
         long startTimeInNanos = System.nanoTime();
         if(LOGGER.isLoggable(Level.FINER)) {
@@ -126,7 +124,7 @@ public class DownstreamPipelineTriggerRunListener extends RunListener<WorkflowRu
             return;
         }
 
-        WorkflowJob upstreamPipeline = upstreamBuild.getParent();
+        Job<?, ?> upstreamPipeline = upstreamBuild.getParent();
 
         String upstreamPipelineFullName = upstreamPipeline.getFullName();
         int upstreamBuildNumber = upstreamBuild.getNumber();
@@ -173,7 +171,7 @@ public class DownstreamPipelineTriggerRunListener extends RunListener<WorkflowRu
                     continue;
                 }
 
-                final WorkflowJob downstreamPipeline = Jenkins.get().getItemByFullName(downstreamPipelineFullName, WorkflowJob.class);
+                final Job<?, ?> downstreamPipeline = Jenkins.get().getItemByFullName(downstreamPipelineFullName, Job.class);
                 if (downstreamPipeline == null || downstreamPipeline.getLastBuild() == null) {
                     LOGGER.log(Level.FINE, "Downstream pipeline {0} or downstream pipeline last build not found from upstream build {1}. Database synchronization issue or security restriction?",
                             new Object[]{downstreamPipelineFullName, upstreamBuild.getFullDisplayName(), Jenkins.getAuthentication()});
@@ -216,7 +214,7 @@ public class DownstreamPipelineTriggerRunListener extends RunListener<WorkflowRu
                 for (String transitiveUpstreamPipelineName : transitiveUpstreamPipelines.keySet()) {
                     // Skip if one of the downstream's upstream is already building or in queue
                     // Then it will get triggered anyway by that upstream, we don't need to trigger it again
-                    WorkflowJob transitiveUpstreamPipeline = Jenkins.get().getItemByFullName(transitiveUpstreamPipelineName, WorkflowJob.class);
+                    Job<?, ?> transitiveUpstreamPipeline = Jenkins.get().getItemByFullName(transitiveUpstreamPipelineName, Job.class);
 
                     if (transitiveUpstreamPipeline == null) {
                         // security: not allowed to view this transitive upstream pipeline, continue to loop
@@ -244,12 +242,12 @@ public class DownstreamPipelineTriggerRunListener extends RunListener<WorkflowRu
 
                 if (!downstreamPipeline.isBuildable()) {
                     LOGGER.log(Level.FINE, "Skip triggering of non buildable (disabled: {0}, isHoldOffBuildUntilSave: {1}) downstream pipeline {2} from upstream build {3}",
-                            new Object[]{downstreamPipeline.isDisabled(), downstreamPipeline.isHoldOffBuildUntilSave(), downstreamPipeline.getFullName(), upstreamBuild.getFullDisplayName()});
+                            new Object[]{((ParameterizedJobMixIn.ParameterizedJob<?, ?>) downstreamPipeline).isDisabled(), downstreamPipeline.isHoldOffBuildUntilSave(), downstreamPipeline.getFullName(), upstreamBuild.getFullDisplayName()});
                     rejectedPipelines.add(downstreamPipelineFullName);
                     continue;
                 }
 
-                WorkflowJobDependencyTrigger downstreamPipelineTrigger = this.globalPipelineMavenConfig.getPipelineTriggerService().getWorkflowJobDependencyTrigger(downstreamPipeline);
+                WorkflowJobDependencyTrigger downstreamPipelineTrigger = this.globalPipelineMavenConfig.getPipelineTriggerService().getWorkflowJobDependencyTrigger((ParameterizedJobMixIn.ParameterizedJob<?, ?>) downstreamPipeline);
                 if (downstreamPipelineTrigger == null) {
                     LOGGER.log(Level.FINE, "Skip triggering of downstream pipeline {0} from upstream build {1}: dependency trigger not configured", new Object[]{downstreamPipeline.getFullName(), upstreamBuild.getFullDisplayName()});
                     rejectedPipelines.add(downstreamPipelineFullName);

jenkins-plugin/src/main/java/org/jenkinsci/plugins/pipeline/maven/service/PipelineTriggerService.java
@@ -23,7 +23,6 @@ import org.jenkinsci.plugins.pipeline.maven.cause.MavenDependencyCause;
 import org.jenkinsci.plugins.pipeline.maven.cause.MavenDependencyCauseHelper;
 import org.jenkinsci.plugins.pipeline.maven.dao.PipelineMavenPluginDao;
 import org.jenkinsci.plugins.pipeline.maven.trigger.WorkflowJobDependencyTrigger;
-import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 
 import javax.annotation.CheckForNull;
 import javax.annotation.Nonnull;
@@ -116,7 +115,7 @@ public class PipelineTriggerService {
                     continue;
                 }
 
-                final WorkflowJob downstreamPipeline = Jenkins.get().getItemByFullName(downstreamPipelineFullName, WorkflowJob.class);
+                final Job<?, ?> downstreamPipeline = Jenkins.get().getItemByFullName(downstreamPipelineFullName, Job.class);
                 if (downstreamPipeline == null || downstreamPipeline.getLastBuild() == null) {
                     if (logger.isLoggable(Level.FINE)) {
                         logger.log(Level.FINE, "Downstream pipeline " + downstreamPipelineFullName + " or downstream pipeline last build not found. Database synchronization issue or security restriction?");
@@ -132,7 +131,7 @@ public class PipelineTriggerService {
                 for (String transitiveUpstreamPipelineName : transitiveUpstreamPipelines.keySet()) {
                     // Skip if one of the downstream's upstream is already building or in queue
                     // Then it will get triggered anyway by that upstream, we don't need to trigger it again
-                    WorkflowJob transitiveUpstreamPipeline = Jenkins.get().getItemByFullName(transitiveUpstreamPipelineName, WorkflowJob.class);
+                    Job<?, ?> transitiveUpstreamPipeline = Jenkins.get().getItemByFullName(transitiveUpstreamPipelineName, Job.class);
 
                     if (transitiveUpstreamPipeline == null) {
                         // security: not allowed to view this transitive upstream pipeline, continue to loop
@@ -157,14 +156,15 @@ public class PipelineTriggerService {
 
                 if (!downstreamPipeline.isBuildable()) {
                     if (logger.isLoggable(Level.FINE)) {
-                        logger.log(Level.FINE, "Skip triggering of non buildable (disabled: " + downstreamPipeline.isDisabled() +
-                                ", isHoldOffBuildUntilSave: " + downstreamPipeline.isHoldOffBuildUntilSave() +
+                        logger.log(Level.FINE, "Skip triggering of non buildable (" +
+                                (downstreamPipeline instanceof ParameterizedJobMixIn.ParameterizedJob ? ("disabled: " + ((ParameterizedJobMixIn.ParameterizedJob<?, ?>) downstreamPipeline).isDisabled() + ", ") : "") +
+                                "isHoldOffBuildUntilSave: " + downstreamPipeline.isHoldOffBuildUntilSave() +
                                 ") downstream pipeline " + downstreamPipeline.getFullName());
                     }
                     continue;
                 }
 
-                WorkflowJobDependencyTrigger downstreamPipelineTrigger = this.globalPipelineMavenConfig.getPipelineTriggerService().getWorkflowJobDependencyTrigger(downstreamPipeline);
+                WorkflowJobDependencyTrigger downstreamPipelineTrigger = this.globalPipelineMavenConfig.getPipelineTriggerService().getWorkflowJobDependencyTrigger((ParameterizedJobMixIn.ParameterizedJob<?, ?>) downstreamPipeline);
                 if (downstreamPipelineTrigger == null) {
                     LOGGER.log(Level.FINE, "Skip triggering of downstream pipeline {0}: dependency trigger not configured", new Object[]{downstreamPipeline.getFullName()});
                     continue;
@@ -311,13 +311,13 @@ public class PipelineTriggerService {
         return null;
     }
 
-    public boolean isUpstreamBuildVisibleByDownstreamBuildAuth(@Nonnull WorkflowJob upstreamPipeline, @Nonnull WorkflowJob downstreamPipeline) {
-        Authentication downstreamPipelineAuth = Tasks.getAuthenticationOf(downstreamPipeline);
+    public boolean isUpstreamBuildVisibleByDownstreamBuildAuth(@Nonnull Job<?, ?> upstreamPipeline, @Nonnull Job<?, ?>  downstreamPipeline) {
+        Authentication downstreamPipelineAuth = Tasks.getAuthenticationOf((Queue.FlyweightTask) downstreamPipeline);
 
         // see https://github.com/jenkinsci/jenkins/blob/jenkins-2.176.2/core/src/main/java/jenkins/triggers/ReverseBuildTrigger.java#L132
         // jenkins.triggers.ReverseBuildTrigger#shouldTrigger
         try (ACLContext ignored = ACL.as(downstreamPipelineAuth)) {
-            WorkflowJob upstreamPipelineObtainedAsImpersonated = getItemByFullName(upstreamPipeline.getFullName(), WorkflowJob.class);
+            Job<?, ?>  upstreamPipelineObtainedAsImpersonated = getItemByFullName(upstreamPipeline.getFullName(), Job.class);
             boolean result = upstreamPipelineObtainedAsImpersonated != null;
             if (LOGGER.isLoggable(Level.FINE)) {
                 LOGGER.log(Level.FINE, "isUpstreamBuildVisibleByDownstreamBuildAuth(upstreamPipeline: {0}, downstreamPipeline: {1}): downstreamPipelineAuth: {2}, upstreamPipelineObtainedAsImpersonated:{3}, result: {4}",

jenkins-plugin/src/test/java/org/jenkinsci/plugins/pipeline/maven/listeners/DownstreamPipelineTriggerRunListenerTest.java
@@ -250,7 +250,7 @@ public class DownstreamPipelineTriggerRunListenerTest {
             when(dao.listDownstreamJobsByArtifact("pipeline", 42))
                     .thenReturn(Collections.singletonMap(new MavenArtifact("groupId:artifactId:jar:version"),
                             new TreeSet<>(Collections.singleton("downstream"))));
-            when(jenkins.getItemByFullName("downstream", WorkflowJob.class)).thenReturn(downstream);
+            when(jenkins.getItemByFullName("downstream", Job.class)).thenReturn(downstream);
 
             listener.onCompleted(build, taskListener);
 
@@ -286,7 +286,6 @@ public class DownstreamPipelineTriggerRunListenerTest {
             when(dao.listDownstreamJobsByArtifact("pipeline", 42))
                     .thenReturn(Collections.singletonMap(new MavenArtifact("groupId:upstreamArtifactId:jar:version"),
                             new TreeSet<>(Collections.singleton("downstream"))));
-            when(jenkins.getItemByFullName("downstream", WorkflowJob.class)).thenReturn(downstream);
             when(jenkins.getItemByFullName("downstream", Job.class)).thenReturn(downstream);
             when(dao.getGeneratedArtifacts("downstream", 4242)).thenReturn(
                     Collections.singletonList(new MavenArtifact("groupId:downstreamArtifactId:jar:version")));
@@ -336,7 +335,6 @@ public class DownstreamPipelineTriggerRunListenerTest {
             when(build.getParent()).thenReturn(job);
             when(build.getNumber()).thenReturn(42);
             when(dao.listDownstreamJobsByArtifact("pipeline", 42)).thenReturn(downstreamJobs);
-            when(jenkins.getItemByFullName("downstream", WorkflowJob.class)).thenReturn(downstream);
             when(jenkins.getItemByFullName("downstream", Job.class)).thenReturn(downstream);
             when(dao.getGeneratedArtifacts("downstream", 4242)).thenReturn(
                     Collections.singletonList(new MavenArtifact("groupId:downstreamArtifactId:jar:version")));

commit da3aef82238fffb9b72674e0684b036dc54a6480

bugfix: dependency trigger is not visible anymore in configuration page
regression since #517

jenkins-plugin/src/main/java/org/jenkinsci/plugins/pipeline/maven/listeners/DatabaseSyncItemListener.java
@@ -6,7 +6,7 @@ import hudson.model.ItemGroup;
 import hudson.model.listeners.ItemListener;
 import jenkins.model.Jenkins;
 import org.jenkinsci.plugins.pipeline.maven.GlobalPipelineMavenConfig;
-import org.jenkinsci.plugins.workflow.flow.FlowExecutionOwner;
+import org.jenkinsci.plugins.workflow.flow.BlockableResume;
 
 import javax.inject.Inject;
 import java.util.logging.Level;
@@ -26,7 +26,7 @@ public class DatabaseSyncItemListener extends ItemListener {
 
     @Override
     public void onDeleted(Item item) {
-        if (item instanceof FlowExecutionOwner.Executable) {
+        if (item instanceof BlockableResume) {
             LOGGER.log(Level.FINE, "onDeleted({0})", item);
             globalPipelineMavenConfig.getDao().deleteJob(item.getFullName());
         } else {
@@ -36,7 +36,7 @@ public class DatabaseSyncItemListener extends ItemListener {
 
     @Override
     public void onRenamed(Item item, String oldName, String newName) {
-        if (item instanceof FlowExecutionOwner.Executable) {
+        if (item instanceof BlockableResume) {
             LOGGER.log(Level.FINE, "onRenamed({0}, {1}, {2})", new Object[]{item, oldName, newName});
 
             String oldFullName;
@@ -55,7 +55,7 @@ public class DatabaseSyncItemListener extends ItemListener {
 
     @Override
     public void onLocationChanged(Item item, String oldFullName, String newFullName) {
-        if (item instanceof FlowExecutionOwner.Executable) {
+        if (item instanceof BlockableResume) {
             LOGGER.log(Level.FINE, "onLocationChanged({0}, {1}, {2})", new Object[]{item, oldFullName, newFullName});
             globalPipelineMavenConfig.getDao().renameJob(oldFullName, newFullName);
         } else {

jenkins-plugin/src/main/java/org/jenkinsci/plugins/pipeline/maven/trigger/WorkflowJobDependencyTrigger.java
@@ -7,7 +7,7 @@ import hudson.triggers.TriggerDescriptor;
 import jenkins.branch.MultiBranchProject;
 import jenkins.branch.OrganizationFolder;
 import org.jenkinsci.Symbol;
-import org.jenkinsci.plugins.workflow.flow.FlowExecutionOwner;
+import org.jenkinsci.plugins.workflow.flow.BlockableResume;
 import org.kohsuke.stapler.DataBoundConstructor;
 
 /**
@@ -25,7 +25,7 @@ public class WorkflowJobDependencyTrigger extends Trigger<Item> {
     public static class DescriptorImpl extends TriggerDescriptor {
         @Override
         public boolean isApplicable(Item item) {
-            return item instanceof FlowExecutionOwner.Executable || item instanceof MultiBranchProject || item instanceof OrganizationFolder;
+            return item instanceof BlockableResume || item instanceof MultiBranchProject || item instanceof OrganizationFolder;
         }
 
         public String getDisplayName() {