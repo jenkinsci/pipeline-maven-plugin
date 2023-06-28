f420ef0bf141a97cebb7f9293d5e58f27f4be6f2
Olivier Lamy <olamy@apache.org>
2023-06-28 10:29:18 +0200

    fix this test
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- jenkins-plugin/src/test/java/org/jenkinsci/plugins/pipeline/maven/WithMavenStepNoOptionalsTest.java
+++ jenkins-plugin/src/test/java/org/jenkinsci/plugins/pipeline/maven/WithMavenStepNoOptionalsTest.java
@@ -47,8 +47,9 @@ public class WithMavenStepNoOptionalsTest {
     @Test
     public void maven_build_jar_project_on_master_succeeds() throws Throwable {
         loadMavenJarProjectInGitRepo(gitRepoRule);
-
-        jenkinsRule.then(WithMavenStepNoOptionalsTest::setup, new Build(gitRepoRule.toString()));
+        jenkinsRule.extraEnv("MAVEN_ZIP_PATH", Paths.get("target", "apache-maven-" + MavenUtil.MAVEN_VERSION + "-bin.zip").toAbsolutePath().toString())
+                .extraEnv("MAVEN_VERSION", MavenUtil.MAVEN_VERSION)
+                .then(WithMavenStepNoOptionalsTest::setup, new Build(gitRepoRule.toString()));
     }
 
     private static class Build implements RealJenkinsRule.Step {
