bab8febdf134feb7f0753026ce5ae07bfdd9684d
Adrien Lecharpentier <alecharpentier@cloudbees.com>
2025-03-06 09:04:03 +0100

    Restores and adapts test for JacocoPublisher

---
+++ pipeline-maven/src/test/java/org/jenkinsci/plugins/pipeline/maven/publishers/JacocoReportPublisherTest.java
@@ -0,0 +1,36 @@
+package org.jenkinsci.plugins.pipeline.maven.publishers;
+
+import hudson.model.Result;
+import org.jenkinsci.plugins.pipeline.maven.AbstractIntegrationTest;
+import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import org.jenkinsci.plugins.workflow.job.WorkflowRun;
+import org.junit.jupiter.api.Test;
+
+public class JacocoReportPublisherTest extends AbstractIntegrationTest {
+
+    @Test
+    public void maven_build_jar_with_jacoco_succeeds() throws Exception {
+        loadSourceCodeInGitRepository(
+                this.gitRepoRule,
+                "/org/jenkinsci/plugins/pipeline/maven/test/test_maven_projects/maven_jar_with_jacoco_project/");
+
+        // @formatter:off
+        String pipelineScript = "node() {\n" +
+            "    git($/" + gitRepoRule.toString() + "/$)\n" +
+            "    withMaven() {\n" +
+            "        if (isUnix()) {\n" +
+            "            sh 'mvn package verify'\n" +
+            "        } else {\n" +
+            "            bat 'mvn package verify'\n" +
+            "        }\n" +
+            "    }\n" +
+            "}";
+        // @formatter:on
+
+        WorkflowJob pipeline = jenkinsRule.createProject(WorkflowJob.class, "jar-with-jacoco");
+        pipeline.setDefinition(new CpsFlowDefinition(pipelineScript, true));
+        WorkflowRun build = jenkinsRule.assertBuildStatus(Result.ABORTED, pipeline.scheduleBuild2(0));
+        jenkinsRule.assertLogContains("The jacocoPublisher is deprecated as is the Jacoco plugin and you should not use it", build);
+    }
+}
---
+++ pipeline-maven/src/test/resources/org/jenkinsci/plugins/pipeline/maven/test/test_maven_projects/maven_jar_with_jacoco_project/src/main/java/com/example/MonoModuleMavenApp.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class MonoModuleMavenApp {
+    /**
+     * TODO message
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello MonoModuleMavenApp");
+    }
+}
